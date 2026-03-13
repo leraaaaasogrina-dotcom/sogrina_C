bef8814e94f72f7a1e86d9199dd2a61241a81fcb
lera <leraaaaasogrina@gmail.com>
2026-03-14 00:14:00 +0300

    lab1 task1

---
+++ lab1/task1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a;
+    a = 10;
+    float b;
+    b = 3,14;
+    char c;
+    c = 'A';
+    printf("a = %d \n", a);
+    printf("b = %f \n", b);
+    printf("c = %c \n", c);
+    return 0;
+}
