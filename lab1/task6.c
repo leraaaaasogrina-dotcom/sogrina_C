9db96d69c5669f7c887c24ee3b6aab8c54e34162
lera <leraaaaasogrina@gmail.com>
2026-03-16 09:46:20 +0300

    Revert "Lab1 task1"

--- lab1/task6.c
+++ lab1/task6.c
@@ -1,11 +1,1 @@
-HEAD
-#include <stdio.h>
- 
-int main(void)
-{
-    int a = 5;
-    int b = 2;
-    printf("%1f\n", (float)a/b);
-    return 0;
-}
-2b54c02ad42ba8eeb0eb25d8f7712a2c08a83e4b
+
--- lab1/task9.c
+++ lab1/task9.c
@@ -1,13 +1,1 @@
-HEAD
-#include <stdio.h>
- 
-int main(void)
-{
-    int a;
-    printf("Введите число:");
-    scanf("%d", &a);
-    printf("%d\n", a*a + 2*a + 1);
-    printf("%d\n", (a+1)*(a+1)-(a-1)*(a-1));  
-    return 0;
-}
-2b54c02ad42ba8eeb0eb25d8f7712a2c08a83e4b
+
