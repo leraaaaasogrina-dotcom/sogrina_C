8b2f58c686f864cc5e2d04940f61f7ff75541418
lera <leraaaaasogrina@gmail.com>
2026-04-03 23:45:29 +0300

    hello

--- hello/1.c
+++ hello/1.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
-int main(void)
+struct Student
 {
-    int x = 10;
-    int *px = &x;
-    printf("Addres %p \n", (void *)px);
-    printf("Значение: %d \n", *px);
+    char name[50];
+    int age;
+    float mark;
+};
+int main()
+{
+    struct Student one;
+    printf("Введите имя-");
+    scanf("%s", one.name);
+    printf("Ввeдите возраст-");
+    scanf("%d", &one.age);
+    printf("Ввeдите оценку-");
+    scanf("%f", &one.mark);
+    printf("Name-%s, Age-%d, Makr-%2f", one.name, one.age, one.mark);
     return 0;
 }
--- hello/2.c
+++ hello/2.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
-int main(void)
+struct Student
+{   
+    float mark;
+    char name[50];
+}; 
+int main()
 {
-    int x = 10;
+    struct Student people[5] = { {5.3, "Tom"}, {9.1, "Bob"}, {1.9, "Alice"}, {8.0, "Bark"}, {4.4, "Sam"}};
+    int max=0;
+    int i;
+   
+    for(i = 1; i < 5; i++)
+    {
+        if(people[i].mark>people[max].mark)
+        {
+            max=i;
+        }
+    } 
+    printf("Список всех студентов:\n");
+    for(i = 0; i<5; i++)
+    {
+        printf("%d Имя: %s, Оценка: %.2f\n", i+1, people[i].name, people[i].mark);
+    }
     
-    int *px = &x;
-    printf("Variable до:=%d\n", *px);
-    *px=22;
-    printf("Variable после:=%d\n", *px);
+    printf("Студент с наибольшим баллом:\n");
+    printf("Имя: %s\n", people[max].name);
+    printf("Оценка: %.2f\n", people[max].mark);
     return 0;
 }
